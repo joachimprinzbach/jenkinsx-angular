4a6514a2a095e43b54fd36ef180596e6cc38680e
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2019-10-25 13:58:47 +0200

    add text to title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular.io jenkins-x kickstart';
+  title = 'angular.io jenkins-x kickstart - preview env';
 }
